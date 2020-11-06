8b3d60a1182e63a0f88ea591020ec18a686a37ce
jeongkyojun <kyojun2002@naver.com>
2020-11-06 22:08:22 +0900

    day 2. 그림보기(미완)

---
+++ C#Project/Task2_ImgViewer/Task2_ImgViewer/FrmViewer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2_ImgViewer
+{
+    public partial class FrmViewer : Form
+    {
+        public FrmViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmViewer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstDir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private int SetDrive()
+        {
+            String[] strDrives;
+            String strTmp;
+
+            // Up항목을 추가한다.
+            lstDir.Items.Add("...", 0);
+
+            // 드라이브 목록을 얻는다.
+            strDrives = System.IO.Directory.GetLogicalDrives();
+
+            // 얻어진 드라이브 목록을 리스트뷰에 입력한다.
+            foreach(string str in strDrives)
+            {
+                strTmp = str.Remove(2,1);
+                lstDir.Items.Add(strTmp, 1);
+            }
+            return lstDir.Items.Count;
+        }
+
+        // 하위 폴더를 화면에 출력한다.
+        // 반환값 : 성공, 실패
+        private bool SetFolder(String strParentPath)
+        {
+            System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(strParentPath);
+
+            // 리스트뷰의 아이템을 모두 삭제한 후
+            lstDir.Items.Clear();
+
+            // 드라이브 정보를 보여준다.
+            m_nCnt = SetDrive();
+            lblPath.Text = "";
+
+            try
+            {
+                foreach (System.IO.DirectoryInfo dirInfoCurDir in dirInfo.GetDirectories("*"))
+                {
+                    lstDir.Items.Add(dirInfoCurDir.Name.ToString(), 2);
+                }
+                lblPath.Text = strParentPath.Remove(strParentPath.Length - 1, 1);
+            }
+            catch
+            {
+                MessageBox.Show("접근에 실패하였습니다.");
+                return false;
+            }
+            return true;
+        }
+
+        private void lstDir_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
